b7beb4f905ee6be3ab6dae500d9af2271867c1b2
francisco de asís domínguez iceta <ciscoadiz@gmail.com>
2026-04-14 20:03:45 +0100

    test(gateway-service): add integration tests for routing and JWT filter
    Tests cover auth login routing to auth-service via WireMock, JWT filter
    blocking protected endpoints without token, public cats endpoint routing
    and unknown route returning 401. WireMock DevService simulates all
    internal services.

---
+++ gateway-service/src/test/java/org/ciscoadiz/gateway/resource/GatewayResourceTest.java
@@ -0,0 +1,89 @@
+package org.ciscoadiz.gateway.resource;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import io.quarkiverse.wiremock.devservice.ConnectWireMock;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.hamcrest.Matchers.equalTo;
+
+@QuarkusTest
+@ConnectWireMock
+class GatewayResourceTest {
+
+    WireMock wiremock;
+
+    @BeforeEach
+    void setUp() {
+        wiremock.resetToDefaultMappings();
+    }
+
+    @Test
+    void testLoginRoutedToAuthService() {
+        wiremock.register(post(urlEqualTo("/auth/login"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("""
+                            {
+                                "accessToken": "test-token",
+                                "refreshToken": "test-refresh"
+                            }
+                            """)));
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("""
+                {
+                    "email": "test@kittigram.org",
+                    "password": "password123"
+                }
+                """)
+                .when()
+                .post("/api/auth/login")
+                .then()
+                .statusCode(200)
+                .body("accessToken", equalTo("test-token"));
+    }
+
+    @Test
+    void testProtectedEndpointWithoutToken() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/cats")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    void testPublicCatsEndpointRouted() {
+        wiremock.register(get(urlEqualTo("/cats"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[]")));
+
+        given()
+                .when()
+                .get("/api/cats")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void testUnknownRouteRequiresAuth() {
+        given()
+                .when()
+                .get("/api/unknown")
+                .then()
+                .statusCode(401);
+    }
+}
